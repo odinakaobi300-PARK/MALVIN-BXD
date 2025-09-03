083dc9c402e1eb71111dfdbbd57c42d1243016ee
odinakaobi300-PARK <odinakaobi300@gmail.com>
2025-09-03 18:29:03 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~k6chHZIL#hGhFD_bArM3_anr5INhs0O83e5DmAWrBpoUaItAtIKk",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2349134519459", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
